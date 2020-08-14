c5e6a18d761b2a567c26016210b9fac0600170f7
Awet <awetkifle94@gmail.com>
2020-08-14 16:25:31 +0100

    update handler and install dependencies

--- handler.js
+++ handler.js
@@ -1,18 +1,46 @@
 "use strict";
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
-module.exports.createBooking = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.createBooking = (event, context, callback) => {
+  const bookingInfo = JSON.parse(event.body);
+
+  const { booking_id, booking_email } = bookingInfo;
+
+  var params = {
+    TableName: "bookings",
+    Item: {
+      booking_id: { S: booking_id },
+      booking_email: { S: booking_email },
+    },
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log("params: ", params);
+  // Call DynamoDB to add the item to the table
+  ddb.putItem(params, function (err, data) {
+    console.log("response: ", err, data);
+    if (err) {
+      console.log("Error", err);
+      let response = {
+        statusCode: "500",
+        body: "fail",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      context.fail(response);
+    } else {
+      console.log("Success", data);
+      let response = {
+        statusCode: "200",
+        body: "success",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      context.succeed(response);
+    }
+  });
 };
